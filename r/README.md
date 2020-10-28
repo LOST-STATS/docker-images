dbbf83c0c41bc92550b16efa11e40a20e304b78f
Kevin Wilson <khwilson@gmail.com>
2020-10-28 12:30:06 -0400

    Add tester-r image

---
+++ r/README.md
@@ -0,0 +1,6 @@
+# Adding dependencies to the R image
+
+We use `renv` to manage our R dependencies. To update the dependencies you should:
+  * Add them to `requirements.R` in the `renv::install` section
+  * Run the following: `docker run --rm -it -v "$(pwd)/renv.lock:/home/yogi/renv.lock -v "$(pwd)/requirements.R:/home/yogi/requirements.R" ghcr.io/khwilson/tester-r:latest R -e "source('requirements.R')"`
+  * Then commit the updated `renv.lock` and `requirements.R` files in this folder
---
+++ r/requirements.R
@@ -0,0 +1,19 @@
+#' This file sets up our renv environment. If you'd like to add
+#' any extra dependencies, add them in the renv::install section below
+#' After that run::
+#'
+#'  docker run --rm -v renv.lock:/home/yogi/renv.lock requirements.R:/home/yogi/requirements.R ghcr.io/khwilson/tester-r Rscript --vanilla -e 'source("requirements.R")''
+#'
+#' Note that this should _only_ be run when you want to update the
+#' renv.lock file. Otherwise, you should use renv::restore()
+
+# Save all the packages that are in our library
+renv::settings$snapshot.type("simple")
+
+renv::install(c(
+    "tidyverse",
+    "lubridate",
+    "ggplot2"
+))
+
+renv::snapshot()
